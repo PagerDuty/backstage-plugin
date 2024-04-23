668c7f79cf8c4cb2df590f2babb5ded504a12cc6
Milenko Tomic <miletomc@gmail.com>
2024-04-23 14:22:31 -0400

    refactor: move common components to its own folder

---
+++ src/components/PagerDutyCardCommon/index.ts
@@ -0,0 +1,13 @@
+import InsightsCard from './InsightsCard';
+import {OpenServiceButton} from './OpenServiceButton';
+import ServiceStandardsCard from './ServiceStandardsCard';
+import StatusCard from './StatusCard';
+import {TriggerIncidentButton} from './TriggerIncidentButton';
+
+export {
+    InsightsCard,
+    OpenServiceButton,
+    ServiceStandardsCard,
+    StatusCard,
+    TriggerIncidentButton
+}
