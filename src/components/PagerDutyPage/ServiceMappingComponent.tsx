c2de0d509f94af9d74b12f90ef2c60840eb8f8d4
Tiago Barbosa <tbarbos@hotmail.com>
2024-07-24 00:25:11 +0100

    chore: refactored areas with JSON.parse on potentially undefined code
    Signed-off-by: Tiago Barbosa <tbarbos@hotmail.com>

--- src/components/PagerDutyPage/ServiceMappingComponent.tsx
+++ src/components/PagerDutyPage/ServiceMappingComponent.tsx
@@ -24,39 +24,50 @@ export const ServiceMappingComponent = () => {
     }
 
     function fetchCatalogEntities() {
-        catalogApi
-          .getEntities({
-            filter: { kind: "Component" },
-          })
-          .then((result) => {
-            const entities: BackstageEntity[] = [];
-            result.items.forEach((entity: any) => {
-              const annotations: Annotations = JSON.parse(
-                JSON.stringify(entity.metadata.annotations)
-              );
+      catalogApi
+        .getEntities({
+          filter: { kind: "Component" },
+        })
+        .then((result) => {
+          const entities: BackstageEntity[] = [];
+          result.items.forEach((entity) => {
+            const annotations: Annotations = {
+              "pagerduty.com/integration-key":
+                entity.metadata?.annotations?.[
+                  "pagerduty.com/integration-key"
+                ] ?? "",
+              "pagerduty.com/service-id":
+                entity.metadata?.annotations?.["pagerduty.com/service-id"] ??
+                "",
+            };
 
-              entities.push({
-                name: entity.metadata?.name,
-                id: entity.metadata?.uid ?? "",
-                namespace: entity.metadata?.namespace ?? "",
-                type: entity.kind ?? "",
-                system: JSON.stringify(entity.spec?.system) || "",
-                owner: JSON.stringify(entity.spec?.owner) || "",
-                lifecycle: JSON.stringify(entity.spec?.lifecycle) || "",
-                annotations: annotations,
-              });
+            entities.push({
+              name: entity.metadata?.name,
+              id: entity.metadata?.uid ?? "",
+              namespace: entity.metadata?.namespace ?? "",
+              type: entity.kind ?? "",
+              system: entity.spec?.system
+                ? JSON.stringify(entity.spec?.system)
+                : "",
+              owner: entity.spec?.owner
+                ? JSON.stringify(entity.spec?.owner)
+                : "",
+              lifecycle: entity.spec?.lifecycle
+                ? JSON.stringify(entity.spec?.lifecycle)
+                : "",
+              annotations: annotations,
             });
-
-            setCatalogEntities(entities);
           });
+
+          setCatalogEntities(entities);
+        });
     }
 
     fetchMappings();
     fetchCatalogEntities();
   }, [catalogApi, pagerDutyApi]);
 
-  
   return (
-      <MappingTable mappings={entityMappings} catalogEntities={catalogEntities}  />
+    <MappingTable mappings={entityMappings} catalogEntities={catalogEntities} />
   );
 };
